70dd80220442b5938bd4269143bae1890af77e0b
Daniel Barreto <dandpb@gmail.com>
2019-07-12 15:35:31 -0300

    fixed restoreDump

--- restoreDumpFromGlacier.sh
+++ restoreDumpFromGlacier.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "USAGE: $0 filename jobId masterKey"
+if [ "$#" -lt 1 ]; then
+    echo "USAGE: $0 filename jobId masterKey vaultName"
     exit 1
 fi
+
 
 
 filename=$1
 jobId=$2
 masterKey=$3
+vaultName=$4
 
+if [ -z "$vaultName" ]; then
 #Configura as variaveis usadads
-. /opt/gradus/conf/grs-variables
-
-SISTEMA=`cut -d - -f1 /etc/hostname`
-CLIENTE=`cut -d - -f2 /etc/hostname`
-AMBIENTE=`cut -d - -f3 /etc/hostname`
-VAULT_NAME=${SISTEMA}-${CLIENTE}-backups-001
+    . /opt/gradus/conf/grs-variables
+    SISTEMA=`cut -d - -f1 /etc/hostname`
+    CLIENTE=`cut -d - -f2 /etc/hostname`
+    AMBIENTE=`cut -d - -f3 /etc/hostname`
+    vaultName=${SISTEMA}-${CLIENTE}-backups-001
+fi
 
 # recuperação glacier
-aws glacier get-job-output --account-id - --vault-name ${VAULT_NAME} --job-id $jobId $filename
+aws glacier get-job-output --account-id - --vault-name $vaultName --job-id $jobId $filename
 
 # descompatar o tar os com arquivos encryptados individualmente
 tar -xvf $filename
@@ -28,7 +31,7 @@ tar -xvf $filename
 for f in *.crypt ; do [ -f $f ] && openssl smime -decrypt -inform DER -in $f -inkey $masterKey > $f-part && rm $f ; done
 
 #juntar todas as partes
-filenameZip =$(echo $filename | sed -e 's/.tar//g')
+filenameZip=$(echo $filename | sed -e 's/.tar//g')
 cat *'-part-'* > $filenameZip
 rm *'-part'*
 
